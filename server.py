f7f1430f1d87068ee73e9ddb5b06e9d00692ad10
Niveditha7989 <chunduruniveditha6@gmail.com>
2026-04-28 09:56:49 +0530

    First commit

--- server.py
+++ server.py
@@ -35,7 +35,7 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 async def read_index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(request=request, name="index.html")
 
 class LoginRequest(BaseModel):
     username: str
